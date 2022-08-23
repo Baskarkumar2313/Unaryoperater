93ab97742679841c59990ade464bccbcf457e31b
BASKAR.K <110029191+Baskarkumar2313@users.noreply.github.com>
2022-08-23 16:52:49 +0530

    Add files via upload

---
+++ Unaryoperater.java
@@ -0,0 +1,18 @@
+class Unaryoperater
+{
+public static void main(String[]args)
+{
+int value1=20;
+int value2=40;
+System.out.println(value1);
+System.out.println(value2);
+System.out.println(++value1);
+System.out.println(++value1);
+System.out.println(value1--);
+System.out.println(value1--);
+System.out.println(++value2);
+System.out.println(++value2);
+System.out.println(value2--);
+System.out.println(value2--);
+}
+}
